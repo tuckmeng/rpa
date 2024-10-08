b313811fee653674d95d0cd8cc5fb264d4590a5d
tuckmeng <tuckmeng@hotmail.com>
2024-10-08 15:02:21 +0800

    Update winss2.cpp

--- Spreadsheet/winss2.cpp
+++ Spreadsheet/winss2.cpp
@@ -1,257 +1,343 @@
 #include <windows.h>
 #include <windowsx.h>
 #include <commctrl.h>
+#include <shlwapi.h>
 #include <vector>
 #include <string>
-#include <shlobj.h>
 #include <fstream>
 #include <sstream>
-#include <iomanip>
+#include <algorithm>
 #include <ctime>
-#include <zip.h>
+#include <iomanip>
 
 #pragma comment(lib, "comctl32.lib")
-#pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#pragma comment(lib, "shlwapi.lib")
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 const int INITIAL_ROWS = 100;
 const int INITIAL_COLS = 26;
-const int CELL_WIDTH = 80;
-const int CELL_HEIGHT = 25;
 
 HWND hWnd;
 HWND hGrid;
-std::vector<std::vector<HWND>> cells;
 int current_row = 0;
 int current_col = 0;
 
-std::string get_current_time() {
-    auto t = std::time(nullptr);
-    auto tm = *std::localtime(&t);
-    std::ostringstream oss;
-    oss << std::put_time(&tm, "%Y-%m-%dT%H:%M:%SZ");
-    return oss.str();
-}
+// Function prototypes
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+void CreateGrid(HWND parent);
+void AddRow();
+void AddColumn();
+void MoveFocus(int row, int col);
+void SaveDataXLSX(const char* filename);
+void OnMenuFileNew();
+void OnMenuFileSaveAs();
 
-void save_data_xlsx(const char* filename) {
-    zip_t* zip = zip_open(filename, ZIP_CREATE | ZIP_TRUNCATE, nullptr);
-    if (!zip) {
-        MessageBox(hWnd, "Failed to create XLSX file", "Error", MB_ICONERROR);
-        return;
-    }
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    // Initialize common controls
+    INITCOMMONCONTROLSEX icex;
+    icex.dwSize = sizeof(INITCOMMONCONTROLSEX);
+    icex.dwICC = ICC_WIN95_CLASSES;
+    InitCommonControlsEx(&icex);
 
-    // Create [Content_Types].xml
-    std::string content_types = 
-        "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-        "<Types xmlns=\"http://schemas.openxmlformats.org/package/2006/content-types\">"
-        "<Default Extension=\"rels\" ContentType=\"application/vnd.openxmlformats-package.relationships+xml\"/>"
-        "<Default Extension=\"xml\" ContentType=\"application/xml\"/>"
-        "<Override PartName=\"/xl/workbook.xml\" ContentType=\"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml\"/>"
-        "<Override PartName=\"/xl/worksheets/sheet1.xml\" ContentType=\"application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml\"/>"
-        "</Types>";
-    zip_source_t* s = zip_source_buffer(zip, content_types.c_str(), content_types.size(), 0);
-    zip_file_add(zip, "[Content_Types].xml", s, ZIP_FL_OVERWRITE);
+    // Register the window class
+    const char CLASS_NAME[] = "Simple Spreadsheet Class";
 
-    // Create _rels/.rels
-    std::string rels = 
-        "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-        "<Relationships xmlns=\"http://schemas.openxmlformats.org/package/2006/relationships\">"
-        "<Relationship Id=\"rId1\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument\" Target=\"xl/workbook.xml\"/>"
-        "</Relationships>";
-    s = zip_source_buffer(zip, rels.c_str(), rels.size(), 0);
-    zip_file_add(zip, "_rels/.rels", s, ZIP_FL_OVERWRITE);
-
-    // Create xl/workbook.xml
-    std::string workbook = 
-        "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-        "<workbook xmlns=\"http://schemas.openxmlformats.org/spreadsheetml/2006/main\" xmlns:r=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships\">"
-        "<sheets><sheet name=\"Sheet1\" sheetId=\"1\" r:id=\"rId1\"/></sheets>"
-        "</workbook>";
-    s = zip_source_buffer(zip, workbook.c_str(), workbook.size(), 0);
-    zip_file_add(zip, "xl/workbook.xml", s, ZIP_FL_OVERWRITE);
-
-    // Create xl/_rels/workbook.xml.rels
-    std::string workbook_rels = 
-        "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-        "<Relationships xmlns=\"http://schemas.openxmlformats.org/package/2006/relationships\">"
-        "<Relationship Id=\"rId1\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet\" Target=\"worksheets/sheet1.xml\"/>"
-        "</Relationships>";
-    s = zip_source_buffer(zip, workbook_rels.c_str(), workbook_rels.size(), 0);
-    zip_file_add(zip, "xl/_rels/workbook.xml.rels", s, ZIP_FL_OVERWRITE);
-
-    // Create xl/worksheets/sheet1.xml
-    std::ostringstream sheet;
-    sheet << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-          << "<worksheet xmlns=\"http://schemas.openxmlformats.org/spreadsheetml/2006/main\">"
-          << "<sheetData>";
-
-    for (size_t i = 0; i < cells.size(); ++i) {
-        bool row_has_data = false;
-        std::ostringstream row;
-        row << "<row r=\"" << (i + 1) << "\">";
-        
-        for (size_t j = 0; j < cells[i].size(); ++j) {
-            char buffer[256];
-            GetWindowTextA(cells[i][j], buffer, 256);
-            if (buffer[0] != '\0') {
-                row_has_data = true;
-                char col_name = 'A' + j;
-                row << "<c r=\"" << col_name << (i + 1) << "\" t=\"inlineStr\">"
-                    << "<is><t>" << buffer << "</t></is></c>";
-            }
-        }
-        
-        row << "</row>";
-        if (row_has_data) {
-            sheet << row.str();
-        }
-    }
-
-    sheet << "</sheetData></worksheet>";
-    std::string sheet_content = sheet.str();
-    s = zip_source_buffer(zip, sheet_content.c_str(), sheet_content.size(), 0);
-    zip_file_add(zip, "xl/worksheets/sheet1.xml", s, ZIP_FL_OVERWRITE);
-
-    zip_close(zip);
-}
-
-void save_data() {
-    OPENFILENAME ofn;
-    char szFileName[MAX_PATH] = "";
-
-    ZeroMemory(&ofn, sizeof(ofn));
-    ofn.lStructSize = sizeof(ofn);
-    ofn.hwndOwner = hWnd;
-    ofn.lpstrFilter = "Excel Files (*.xlsx)\0*.xlsx\0All Files (*.*)\0*.*\0";
-    ofn.lpstrFile = szFileName;
-    ofn.nMaxFile = MAX_PATH;
-    ofn.Flags = OFN_EXPLORER | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT;
-    ofn.lpstrDefExt = "xlsx";
-
-    if (GetSaveFileName(&ofn)) {
-        save_data_xlsx(szFileName);
-    }
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            return 0;
-        case WM_COMMAND:
-            if (LOWORD(wParam) == 1) {  // Assuming 1 is the ID for the "Save" button
-                save_data();
-            }
-            return 0;
-        case WM_KEYDOWN:
-            switch (wParam) {
-                case VK_TAB:
-                    if (GetAsyncKeyState(VK_SHIFT) & 0x8000) {
-                        // Shift+Tab pressed
-                        if (current_col > 0) {
-                            current_col--;
-                        } else if (current_row > 0) {
-                            current_row--;
-                            current_col = INITIAL_COLS - 1;
-                        }
-                    } else {
-                        // Tab pressed
-                        if (current_col < INITIAL_COLS - 1) {
-                            current_col++;
-                        } else if (current_row < INITIAL_ROWS - 1) {
-                            current_row++;
-                            current_col = 0;
-                        }
-                    }
-                    SetFocus(cells[current_row][current_col]);
-                    return 0;
-                case VK_RETURN:
-                    if (current_row < INITIAL_ROWS - 1) {
-                        current_row++;
-                        SetFocus(cells[current_row][current_col]);
-                    }
-                    return 0;
-            }
-            break;
-    }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
-}
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    WNDCLASSEX wc = {0};
-    wc.cbSize = sizeof(WNDCLASSEX);
-    wc.style = CS_HREDRAW | CS_VREDRAW;
+    WNDCLASS wc = {};
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.lpszClassName = CLASS_NAME;
     wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wc.lpszClassName = "SpreadsheetClass";
-    RegisterClassEx(&wc);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 
-    RECT wr = {0, 0, CELL_WIDTH * INITIAL_COLS, CELL_HEIGHT * INITIAL_ROWS + 50};  // +50 for the save button
-    AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
+    RegisterClass(&wc);
 
+    // Create the window
     hWnd = CreateWindowEx(
-        0,
-        "SpreadsheetClass",
-        "Simple Spreadsheet",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT,
-        wr.right - wr.left, wr.bottom - wr.top,
-        NULL,
-        NULL,
-        hInstance,
-        NULL
+        0,                              // Optional window styles
+        CLASS_NAME,                     // Window class
+        "Simple Spreadsheet",           // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+
+        NULL,       // Parent window    
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
     );
 
-    if (hWnd == NULL) {
+    if (hWnd == NULL)
+    {
         return 0;
     }
 
-    HWND hSaveButton = CreateWindow(
-        "BUTTON",
-        "Save",
-        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-        10,
-        CELL_HEIGHT * INITIAL_ROWS + 10,
-        100,
-        30,
-        hWnd,
-        (HMENU)1,  // ID for the button
-        hInstance,
-        NULL
-    );
-
-    cells.resize(INITIAL_ROWS, std::vector<HWND>(INITIAL_COLS));
-
-    for (int i = 0; i < INITIAL_ROWS; ++i) {
-        for (int j = 0; j < INITIAL_COLS; ++j) {
-            cells[i][j] = CreateWindowEx(
-                WS_EX_CLIENTEDGE,
-                "EDIT",
-                "",
-                WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL,
-                j * CELL_WIDTH,
-                i * CELL_HEIGHT,
-                CELL_WIDTH,
-                CELL_HEIGHT,
-                hWnd,
-                NULL,
-                hInstance,
-                NULL
-            );
-        }
-    }
-
     ShowWindow(hWnd, nCmdShow);
-    UpdateWindow(hWnd);
 
-    SetFocus(cells[0][0]);
-
+    // Run the message loop
     MSG msg = {};
-    while (GetMessage(&msg, NULL, 0, 0)) {
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
 
     return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_CREATE:
+    {
+        // Create menu
+        HMENU hMenu = CreateMenu();
+        HMENU hFileMenu = CreatePopupMenu();
+
+        AppendMenu(hFileMenu, MF_STRING, 1, "New");
+        AppendMenu(hFileMenu, MF_STRING, 2, "Save As");
+        AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, "File");
+
+        SetMenu(hwnd, hMenu);
+
+        // Create grid
+        CreateGrid(hwnd);
+        MoveFocus(0, 0);
+        return 0;
+    }
+
+    case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        switch (wmId)
+        {
+        case 1:
+            OnMenuFileNew();
+            break;
+        case 2:
+            OnMenuFileSaveAs();
+            break;
+        }
+        return 0;
+    }
+
+    case WM_DESTROY:
+        SaveDataXLSX("auto_save.xlsx");
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_SIZE:
+    {
+        RECT rcClient;
+        GetClientRect(hwnd, &rcClient);
+        SetWindowPos(hGrid, NULL, 0, 0, rcClient.right, rcClient.bottom, SWP_NOZORDER);
+        return 0;
+    }
+
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+void CreateGrid(HWND parent)
+{
+    hGrid = CreateWindowEx(
+        0,
+        WC_LISTVIEW,
+        "",
+        WS_CHILD | WS_VISIBLE | LVS_REPORT | LVS_EDITLABELS,
+        0, 0, 0, 0,
+        parent,
+        NULL,
+        (HINSTANCE)GetWindowLongPtr(parent, GWLP_HINSTANCE),
+        NULL);
+
+    // Add columns
+    for (int i = 0; i < INITIAL_COLS; ++i)
+    {
+        LVCOLUMN lvc;
+        lvc.mask = LVCF_TEXT | LVCF_WIDTH;
+        lvc.cx = 80;
+        char colName[3] = { (char)('A' + i), 0 };
+        lvc.pszText = colName;
+        ListView_InsertColumn(hGrid, i, &lvc);
+    }
+
+    // Add rows
+    for (int i = 0; i < INITIAL_ROWS; ++i)
+    {
+        ListView_InsertItem(hGrid, &LVITEM{ .iItem = i });
+    }
+}
+
+void AddRow()
+{
+    int newRow = ListView_GetItemCount(hGrid);
+    ListView_InsertItem(hGrid, &LVITEM{ .iItem = newRow });
+}
+
+void AddColumn()
+{
+    int newCol = Header_GetItemCount(ListView_GetHeader(hGrid));
+    LVCOLUMN lvc;
+    lvc.mask = LVCF_TEXT | LVCF_WIDTH;
+    lvc.cx = 80;
+    char colName[3] = { (char)('A' + newCol), 0 };
+    lvc.pszText = colName;
+    ListView_InsertColumn(hGrid, newCol, &lvc);
+}
+
+void MoveFocus(int row, int col)
+{
+    if (row < 0 || col < 0) return;
+
+    while (row >= ListView_GetItemCount(hGrid)) AddRow();
+    while (col >= Header_GetItemCount(ListView_GetHeader(hGrid))) AddColumn();
+
+    ListView_SetItemState(hGrid, row, LVIS_FOCUSED | LVIS_SELECTED, LVIS_FOCUSED | LVIS_SELECTED);
+    ListView_EnsureVisible(hGrid, row, FALSE);
+    ListView_EditLabel(hGrid, row);
+
+    current_row = row;
+    current_col = col;
+}
+
+void SaveDataXLSX(const char* filename)
+{
+    // Create a temporary directory
+    char tempPath[MAX_PATH];
+    GetTempPath(MAX_PATH, tempPath);
+    char tempDir[MAX_PATH];
+    sprintf_s(tempDir, "%sxlsx_temp_%d", tempPath, GetTickCount());
+    CreateDirectory(tempDir, NULL);
+
+    // Create the necessary directories
+    char xlDir[MAX_PATH], worksheetsDir[MAX_PATH];
+    sprintf_s(xlDir, "%s\\xl", tempDir);
+    sprintf_s(worksheetsDir, "%s\\worksheets", xlDir);
+    CreateDirectory(xlDir, NULL);
+    CreateDirectory(worksheetsDir, NULL);
+
+    // Generate content types file
+    std::ofstream contentTypes(std::string(tempDir) + "\\" + "[Content_Types].xml");
+    contentTypes << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                 << "<Types xmlns=\"http://schemas.openxmlformats.org/package/2006/content-types\">"
+                 << "<Default Extension=\"rels\" ContentType=\"application/vnd.openxmlformats-package.relationships+xml\"/>"
+                 << "<Default Extension=\"xml\" ContentType=\"application/xml\"/>"
+                 << "<Override PartName=\"/xl/workbook.xml\" ContentType=\"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml\"/>"
+                 << "<Override PartName=\"/xl/worksheets/sheet1.xml\" ContentType=\"application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml\"/>"
+                 << "</Types>";
+    contentTypes.close();
+
+    // Generate _rels/.rels file
+    CreateDirectory((std::string(tempDir) + "\\_rels").c_str(), NULL);
+    std::ofstream rels(std::string(tempDir) + "\\_rels\\.rels");
+    rels << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+         << "<Relationships xmlns=\"http://schemas.openxmlformats.org/package/2006/relationships\">"
+         << "<Relationship Id=\"rId1\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument\" Target=\"xl/workbook.xml\"/>"
+         << "</Relationships>";
+    rels.close();
+
+    // Generate xl/workbook.xml file
+    std::ofstream workbook(std::string(xlDir) + "\\workbook.xml");
+    workbook << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+             << "<workbook xmlns=\"http://schemas.openxmlformats.org/spreadsheetml/2006/main\" xmlns:r=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships\">"
+             << "<sheets>"
+             << "<sheet name=\"Sheet1\" sheetId=\"1\" r:id=\"rId1\"/>"
+             << "</sheets>"
+             << "</workbook>";
+    workbook.close();
+
+    // Generate xl/_rels/workbook.xml.rels file
+    CreateDirectory((std::string(xlDir) + "\\_rels").c_str(), NULL);
+    std::ofstream workbookRels(std::string(xlDir) + "\\_rels\\workbook.xml.rels");
+    workbookRels << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                 << "<Relationships xmlns=\"http://schemas.openxmlformats.org/package/2006/relationships\">"
+                 << "<Relationship Id=\"rId1\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet\" Target=\"worksheets/sheet1.xml\"/>"
+                 << "</Relationships>";
+    workbookRels.close();
+
+    // Generate xl/worksheets/sheet1.xml file
+    std::ofstream sheet(std::string(worksheetsDir) + "\\sheet1.xml");
+    sheet << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+          << "<worksheet xmlns=\"http://schemas.openxmlformats.org/spreadsheetml/2006/main\">"
+          << "<sheetData>";
+
+    int rowCount = ListView_GetItemCount(hGrid);
+    int colCount = Header_GetItemCount(ListView_GetHeader(hGrid));
+
+    for (int i = 0; i < rowCount; ++i)
+    {
+        bool rowHasContent = false;
+        std::stringstream rowContent;
+        rowContent << "<row r=\"" << (i + 1) << "\">";
+
+        for (int j = 0; j < colCount; ++j)
+        {
+            char cellContent[256];
+            ListView_GetItemText(hGrid, i, j, cellContent, sizeof(cellContent));
+            if (cellContent[0] != '\0')
+            {
+                rowHasContent = true;
+                char colName[3] = { (char)('A' + j), 0 };
+                rowContent << "<c r=\"" << colName << (i + 1) << "\" t=\"inlineStr\"><is><t>" << cellContent << "</t></is></c>";
+            }
+        }
+
+        rowContent << "</row>";
+
+        if (rowHasContent)
+        {
+            sheet << rowContent.str();
+        }
+    }
+
+    sheet << "</sheetData></worksheet>";
+    sheet.close();
+
+    // Create the XLSX file using 7z
+    char cmd[MAX_PATH * 3];
+    sprintf_s(cmd, "7z.exe a -tzip \"%s\" \"%s\\*\"", filename, tempDir);
+    system(cmd);
+
+    // Clean up temporary directory
+    sprintf_s(cmd, "rmdir /s /q \"%s\"", tempDir);
+    system(cmd);
+}
+
+void OnMenuFileNew()
+{
+    int itemCount = ListView_GetItemCount(hGrid);
+    int colCount = Header_GetItemCount(ListView_GetHeader(hGrid));
+
+    for (int i = 0; i < itemCount; ++i)
+    {
+        for (int j = 0; j < colCount; ++j)
+        {
+            ListView_SetItemText(hGrid, i, j, (LPSTR)"");
+        }
+    }
+
+    MoveFocus(0, 0);
+}
+
+void OnMenuFileSaveAs()
+{
+    char filename[MAX_PATH];
+    OPENFILENAME ofn;
+    ZeroMemory(&filename, sizeof(filename));
+    ZeroMemory(&ofn, sizeof(ofn));
+    ofn.lStructSize = sizeof(ofn);
+    ofn.hwndOwner = hWnd;
+    ofn.lpstrFilter = "Excel Files (*.xlsx)\0*.xlsx\0All Files (*.*)\0*.*\0";
+    ofn.lpstrFile = filename;
+    ofn.nMaxFile = MAX_PATH;
+    ofn.lpstrDefExt = "xlsx";
+    ofn.Flags = OFN_EXPLORER | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT;
+
+    if (GetSaveFileName(&ofn))
+    {
+        SaveDataXLSX(filename);
+    }
 }
